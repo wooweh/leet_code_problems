971783400c329d16ea41255f1110b1c57a869a7a
wooweh <wooweh@proton.me>
2024-01-15 09:15:04 +0200

    mergeSortedArray

---
+++ mergeSortedArray.ts
@@ -0,0 +1,4 @@
+// NO. 88 MERGE SORTED ARRAY
+
+function merge(nums1: number[], m: number, nums2: number[], n: number): void {}
+// TODO: Research how to run individual js function outputs in terminal
